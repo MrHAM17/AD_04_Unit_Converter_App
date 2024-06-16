572540486e036977da71d88ec200a79d25cc882d
MrHAM17 <harshminde1@gmail.com>
2024-06-16 21:24:06 +0530

    Initial commit

---
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.unitconverter;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText kgAsInputValue;
+    Button convert;
+    TextView resultValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+
+        kgAsInputValue = findViewById(R.id.editText);
+        convert = findViewById(R.id.button);
+        resultValue = findViewById(R.id.textView);
+
+        
+
+
+
+
+    }
+
+
+
+
+
+}
